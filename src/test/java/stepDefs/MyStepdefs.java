7606f378a420114c8be139f960f020e1bac3b53e
jjackiemao <150142965+jjackiemao@users.noreply.github.com>
2024-04-18 17:29:02 +0200

    Update MyStepdefs.java

--- src/test/java/stepDefs/MyStepdefs.java
+++ src/test/java/stepDefs/MyStepdefs.java
@@ -10,10 +10,8 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-
 import java.time.Duration;
 import java.util.Random;
-
 import static org.junit.Assert.*;
 
 public class MyStepdefs {
